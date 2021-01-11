6b34c1d9d1a515ff33a2d4df46b9aaa4d41b0e6e
Frank Reust <freust14@gmail.com>
2021-01-10 19:16:37 -0600

    first attempt at findByValue

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -124,5 +125,28 @@ public class JobData {
             e.printStackTrace();
         }
     }
+    public static ArrayList<HashMap<String, String>>  findByValue(String value) {
+        // load data, if not already loaded
+        loadData();
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        for (HashMap<String, String> row : allJobs) {
+
+            for (Map.Entry <String,String> entry: row.entrySet()) {
+               Boolean jobAdded = false;
+                while (!jobAdded) {
+                    if (entry.getKey().contains(value)) {
+                        jobs.add(row);
+                        jobAdded = true;
+                    } else if (entry.getValue().contains(value)) {
+                        jobs.add(row);
+                        jobAdded = true;
+                    }
+                }
+            }
+        }
+        return jobs;
+        }
+
 
 }
+
